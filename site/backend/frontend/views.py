dd110cf30bab361a1975cd0770fdac12010e055b
Robbie Decker <robbd56@gmail.com>
2021-12-09 02:28:28 -0500

    Big updates to UI

--- site/backend/frontend/views.py
+++ site/backend/frontend/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from github import Github, GithubException
 
-github_pat = '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'
+github_pat = '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'
 
 def index(request):
     template = loader.get_template('frontend/index.html')
